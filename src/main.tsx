01819190336afed139418924528a87f0d3ebb59d
Girius Kraucevicius <girius.kraucevicius@oxylabs.io>
2025-09-16 22:23:20 +0300

    feat(Redirect):from root to /films

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Container } from './components/Container.tsx'
 import { Sidebar } from './features/sidebar/Sidebar.tsx'
 import './index.css'
@@ -16,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <QueryClientProvider client={queryClient}>
           <Sidebar />
           <Routes>
+            <Route path="/" element={<Navigate to="/films" replace />} />
             {routes.map(({ path, element }) => (
               <Route key={path} path={path} element={element} />
             ))}
