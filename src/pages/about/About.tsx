4b54f2239eb57737b524aaac5eab827446a343d4
Girius Kraucevicius <girius.kraucevicius@oxylabs.io>
2025-09-16 22:41:44 +0300

    feat(About):page style & content

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,9 @@
 import type { FC } from 'react'
+import { description } from './data'
 
-export const About: FC = () => {
-  return <div>About</div>
-}
+export const About: FC = () => (
+  <div className="flex flex-col p-10 w-[100%] h-full gap-6">
+    <h2 className="text-2xl">About this project</h2>
+    <p>{description}</p>
+  </div>
+)
